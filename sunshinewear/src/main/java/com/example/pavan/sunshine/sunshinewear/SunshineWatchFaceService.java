491806f568fc23a347c6d8efef1168ebf92ae171
pa1-teja <pavanteja.93@gmail.com>
2016-12-05 23:22:46 +0530

    Wearable module successfully added to the Sunshine weather app project.

--- sunshinewear/src/main/java/com/example/pavan/sunshine/sunshinewear/SunshineWatchFaceService.java
+++ sunshinewear/src/main/java/com/example/pavan/sunshine/sunshinewear/SunshineWatchFaceService.java
@@ -22,7 +22,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Rect;
@@ -40,7 +39,6 @@ import android.view.SurfaceHolder;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.wearable.Asset;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.DataEvent;
 import com.google.android.gms.wearable.DataEventBuffer;
@@ -50,7 +48,6 @@ import com.google.android.gms.wearable.DataMap;
 import com.google.android.gms.wearable.DataMapItem;
 import com.google.android.gms.wearable.Wearable;
 
-import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
 
 
@@ -304,33 +301,6 @@ public class SunshineWatchFaceService extends CanvasWatchFaceService {
         }
 
 
-        public Bitmap loadBitmapFromAsset(Asset asset) {
-            if (asset == null) {
-                throw new IllegalArgumentException("Asset must be non-null");
-            }
-            long TIME = 1000;
-            ConnectionResult result =
-                    googleApiClient.blockingConnect(TIME, TimeUnit.MILLISECONDS);
-            if (!result.isSuccess()) {
-                return null;
-            }
-//             convert asset into a file descriptor and block until it's ready
-            InputStream assetInputStream = Wearable.DataApi.getFdForAsset(
-                    googleApiClient, asset).await().getInputStream();
-            googleApiClient.disconnect();
-
-            if (assetInputStream == null) {
-                Log.w(TAG, "Requested an unknown Asset.");
-                return null;
-            }
-            Log.d(TAG, "processed image");
-            // decode the stream into a bitmap
-            return BitmapFactory.decodeStream(assetInputStream);
-        }
-
-
-
-
         @Override
         public void onConnectionSuspended(int i) {
             Log.e(TAG, "suspended GoogleAPI");
@@ -351,219 +321,4 @@ public class SunshineWatchFaceService extends CanvasWatchFaceService {
             super.onDestroy();
         }
     }
-//    private class Engine extends CanvasWatchFaceService.Engine implements
-//            GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
-//        final Handler mUpdateTimeHandler = new EngineHandler(this);
-//        boolean mRegisteredTimeZoneReceiver = false;
-//        Paint mBackgroundPaint;
-//        Paint mTextPaint;
-//        boolean mAmbient;
-//        Calendar mCalendar;
-//        final BroadcastReceiver mTimeZoneReceiver = new BroadcastReceiver() {
-//            @Override
-//            public void onReceive(Context context, Intent intent) {
-//                mCalendar.setTimeZone(TimeZone.getDefault());
-//                invalidate();
-//            }
-//        };
-//        float mXOffset;
-//        float mYOffset;
-//
-//        /**
-//         * Whether the display supports fewer bits for each color in ambient mode. When true, we
-//         * disable anti-aliasing in ambient mode.
-//         */
-//        boolean mLowBitAmbient;
-//
-//        @Override
-//        public void onCreate(SurfaceHolder holder) {
-//            super.onCreate(holder);
-//
-//            setWatchFaceStyle(new WatchFaceStyle.Builder(SunshineWatchFaceService.this)
-//                    .setCardPeekMode(WatchFaceStyle.PEEK_MODE_VARIABLE)
-//                    .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
-//                    .setShowSystemUiTime(false)
-//                    .setAcceptsTapEvents(true)
-//                    .build());
-//            Resources resources = SunshineWatchFaceService.this.getResources();
-//            mYOffset = resources.getDimension(R.dimen.digital_y_offset);
-//
-//            mBackgroundPaint = new Paint();
-//            mBackgroundPaint.setColor(resources.getColor(R.color.background));
-//
-//            mTextPaint = new Paint();
-//            mTextPaint = createTextPaint(resources.getColor(R.color.digital_text));
-//
-//            mCalendar = Calendar.getInstance();
-//        }
-//
-//        @Override
-//        public void onDestroy() {
-//            mUpdateTimeHandler.removeMessages(MSG_UPDATE_TIME);
-//            super.onDestroy();
-//        }
-//
-//        private Paint createTextPaint(int textColor) {
-//            Paint paint = new Paint();
-//            paint.setColor(textColor);
-//            paint.setTypeface(NORMAL_TYPEFACE);
-//            paint.setAntiAlias(true);
-//            return paint;
-//        }
-//
-//        @Override
-//        public void onVisibilityChanged(boolean visible) {
-//            super.onVisibilityChanged(visible);
-//
-//            if (visible) {
-//                registerReceiver();
-//
-//                // Update time zone in case it changed while we weren't visible.
-//                mCalendar.setTimeZone(TimeZone.getDefault());
-//                invalidate();
-//            } else {
-//                unregisterReceiver();
-//            }
-//
-//            // Whether the timer should be running depends on whether we're visible (as well as
-//            // whether we're in ambient mode), so we may need to start or stop the timer.
-//            updateTimer();
-//        }
-//
-//        private void registerReceiver() {
-//            if (mRegisteredTimeZoneReceiver) {
-//                return;
-//            }
-//            mRegisteredTimeZoneReceiver = true;
-//            IntentFilter filter = new IntentFilter(Intent.ACTION_TIMEZONE_CHANGED);
-//            SunshineWatchFaceService.this.registerReceiver(mTimeZoneReceiver, filter);
-//        }
-//
-//        private void unregisterReceiver() {
-//            if (!mRegisteredTimeZoneReceiver) {
-//                return;
-//            }
-//            mRegisteredTimeZoneReceiver = false;
-//            SunshineWatchFaceService.this.unregisterReceiver(mTimeZoneReceiver);
-//        }
-//
-//        @Override
-//        public void onApplyWindowInsets(WindowInsets insets) {
-//            super.onApplyWindowInsets(insets);
-//
-//            // Load resources that have alternate values for round watches.
-//            Resources resources = SunshineWatchFaceService.this.getResources();
-//            boolean isRound = insets.isRound();
-//            mXOffset = resources.getDimension(isRound
-//                    ? R.dimen.digital_x_offset_round : R.dimen.digital_x_offset);
-//            float textSize = resources.getDimension(isRound
-//                    ? R.dimen.digital_text_size_round : R.dimen.digital_text_size);
-//
-//            mTextPaint.setTextSize(textSize);
-//        }
-//
-//        @Override
-//        public void onPropertiesChanged(Bundle properties) {
-//            super.onPropertiesChanged(properties);
-//            mLowBitAmbient = properties.getBoolean(PROPERTY_LOW_BIT_AMBIENT, false);
-//        }
-//
-//        @Override
-//        public void onTimeTick() {
-//            super.onTimeTick();
-//            invalidate();
-//        }
-//
-//        @Override
-//        public void onAmbientModeChanged(boolean inAmbientMode) {
-//            super.onAmbientModeChanged(inAmbientMode);
-//            if (mAmbient != inAmbientMode) {
-//                mAmbient = inAmbientMode;
-//                if (mLowBitAmbient) {
-//                    mTextPaint.setAntiAlias(!inAmbientMode);
-//                }
-//                invalidate();
-//            }
-//
-//            // Whether the timer should be running depends on whether we're visible (as well as
-//            // whether we're in ambient mode), so we may need to start or stop the timer.
-//            updateTimer();
-//        }
-//
-//        /**
-//         * Captures tap event (and tap type) and toggles the background color if the user finishes
-//         * a tap.
-//         */
-//        @Override
-//        public void onTapCommand(int tapType, int x, int y, long eventTime) {
-//            switch (tapType) {
-//                case TAP_TYPE_TOUCH:
-//                    // The user has started touching the screen.
-//                    break;
-//                case TAP_TYPE_TOUCH_CANCEL:
-//                    // The user has started a different gesture or otherwise cancelled the tap.
-//                    break;
-//                case TAP_TYPE_TAP:
-//                    // The user has completed the tap gesture.
-//                    // TODO: Add code to handle the tap gesture.
-//                    Toast.makeText(getApplicationContext(), R.string.message, Toast.LENGTH_SHORT)
-//                            .show();
-//                    break;
-//            }
-//            invalidate();
-//        }
-//
-//        @Override
-//        public void onDraw(Canvas canvas, Rect bounds) {
-//            // Draw the background.
-//            if (isInAmbientMode()) {
-//                canvas.drawColor(Color.BLACK);
-//            } else {
-//                canvas.drawRect(0, 0, bounds.width(), bounds.height(), mBackgroundPaint);
-//            }
-//
-//            // Draw H:MM in ambient mode or H:MM:SS in interactive mode.
-//            long now = System.currentTimeMillis();
-//            mCalendar.setTimeInMillis(now);
-//
-//            String text = mAmbient
-//                    ? String.format("%d:%02d", mCalendar.get(Calendar.HOUR),
-//                    mCalendar.get(Calendar.MINUTE))
-//                    : String.format("%d:%02d:%02d", mCalendar.get(Calendar.HOUR),
-//                    mCalendar.get(Calendar.MINUTE), mCalendar.get(Calendar.SECOND));
-//            canvas.drawText(text, mXOffset, mYOffset, mTextPaint);
-//        }
-//
-//        /**
-//         * Starts the {@link #mUpdateTimeHandler} timer if it should be running and isn't currently
-//         * or stops it if it shouldn't be running but currently is.
-//         */
-//        private void updateTimer() {
-//            mUpdateTimeHandler.removeMessages(MSG_UPDATE_TIME);
-//            if (shouldTimerBeRunning()) {
-//                mUpdateTimeHandler.sendEmptyMessage(MSG_UPDATE_TIME);
-//            }
-//        }
-//
-//        /**
-//         * Returns whether the {@link #mUpdateTimeHandler} timer should be running. The timer should
-//         * only run when we're visible and in interactive mode.
-//         */
-//        private boolean shouldTimerBeRunning() {
-//            return isVisible() && !isInAmbientMode();
-//        }
-//
-//        /**
-//         * Handle updating the time periodically in interactive mode.
-//         */
-//        private void handleUpdateTimeMessage() {
-//            invalidate();
-//            if (shouldTimerBeRunning()) {
-//                long timeMs = System.currentTimeMillis();
-//                long delayMs = INTERACTIVE_UPDATE_RATE_MS
-//                        - (timeMs % INTERACTIVE_UPDATE_RATE_MS);
-//                mUpdateTimeHandler.sendEmptyMessageDelayed(MSG_UPDATE_TIME, delayMs);
-//            }
-//        }
-//    }
 }
